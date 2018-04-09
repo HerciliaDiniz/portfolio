3347e0108006601624bcff5f873f8eef5a40601b
Hercilia Diniz <herciliadiniz@gmail.com>
2018-04-08 17:03:30 -0700

    Implement create and edit

--- app/controllers/portfolio_pages_controller.rb
+++ app/controllers/portfolio_pages_controller.rb
@@ -1,7 +1,45 @@
 class PortfolioPagesController < ApplicationController
 
+  before_action :find_portfolio_item, only: [:show, :edit, :update, :destroy]
+
   def index 
     @portfolio_items = PortfolioPage.all
+  end
+
+  def new
+    @portfolio_item = PortfolioPage.new
+  end 
+
+  def edit
+  end
+
+  def create
+    @portfolio_item = PortfolioPage.new(portfolio_params)
+
+    if @portfolio_item.save
+      redirect_to portfolio_pages_path, notice: 'Your portfolio item is now live.' 
+    else
+      render :new 
+    end
+
+  end
+
+  def update
+    if @portfolio_item.update portfolio_params
+      redirect_to portfolio_pages_path, notice: 'Your portfolio item was successfully updated.' 
+    else  
+      render :new 
+    end
+  end
+
+  private
+
+  def find_portfolio_item
+    @portfolio_item = PortfolioPage.find(params[:id])
+  end 
+
+  def portfolio_params
+    params.require(:portfolio_page).permit(:title, :subtitle, :body)
   end
 
 end
