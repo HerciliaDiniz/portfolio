06eda6d5ad9b0b253f409b319a751bf97ffcfe5c
Hercilia Diniz <herciliadiniz@gmail.com>
2018-04-08 22:13:03 -0700

    Implement custom action toggle_status

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   resources :portfolio_pages, except: [:show]
   get 'portfolio/:id', to: 'portfolio_pages#show', as: 'portfolio_show'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
