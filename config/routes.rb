237e69200c9e32863cec29841f55868f0d09df2f
Hercilia Diniz <herciliadiniz@gmail.com>
2018-06-27 16:27:55 -0700

    Custumized routes for login, logout, and register, and finish authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolio_pages, except: [:show]
   get 'angular', to: 'portfolio_pages#angular'
   get 'rails', to: 'portfolio_pages#rails'
