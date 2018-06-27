b617197866cd5f85ef8ac037d6c46d784a0b0bde
Hercilia Diniz <herciliadiniz@gmail.com>
2018-06-27 16:09:28 -0700

    Implemented basic authentication with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :portfolio_pages, except: [:show]
   get 'angular', to: 'portfolio_pages#angular'
   get 'rails', to: 'portfolio_pages#rails'
